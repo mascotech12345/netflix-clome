c6123ba0a3ebeef9ed30a5dcaf0c9fe29d33d07b
MascoTech <mascotech12345@gmail.com>
2025-11-28 13:34:11 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+alert("Welcome To Netflix. You Can Watch Breath Taking Movies At A Very Cheap Price Only Here On Netflix.")
+// transform is a property in css that has four major values. which are:
+// translate(xpx, ypx), for only one value: translateX(x) or translateY(y).
+// rotate(40deg): for example you can use any other value just make sure its has "deg" in front.
+// scale(x, y), for only one value: scaleX(x) or scaleY(y). 
+// skew(xdeg, ydeg), for only one value: skewX(x) or skewY(y).
+
+
+
+// TRANSITION
+// Transition is a css property that changes the origin state of an element in time and it has other conplexsity 
+// properties of transition.
+// transition-property: any css property
+// transition-duration: in seconds
+// transition-timing-function: linear, ease-in, ease-out, ease-in-out, ease. This is how the transition happens.
+// transition-delay: in seconds
+// short hand for writing transition is:
+// transition: transition-property transition-duration transition-timing-function transition-delay
